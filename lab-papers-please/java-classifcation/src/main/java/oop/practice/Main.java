a1781b0142af8f56031430b34fc4a8763cb9153d
victoriaaa111 <victoria.mutruc@gmail.com>
2024-10-09 20:06:32 +0300

    Base class setup + experimenting with class

--- lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
@@ -1,15 +1,16 @@
 package oop.practice;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+//import java.util.List;
+//import java.util.Scanner;
 
 public class Main {
+  /*
   public static void main(String[] args) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     File inputFile = new File("./lab-papers-please/java-classifcation/src/main/resources/test-input.json");
@@ -49,9 +50,33 @@ public class Main {
     mapper.writeValue(new File("src/main/resources/output/rings.json"), rings);
     mapper.writeValue(new File("src/main/resources/output/marvel.json"), marvel);
   }
+*/
+
+
+  public static void main(String[] args){
+    IndividualTraits individual1 = new IndividualTraits();
+    ArrayList<String> traits1 = new ArrayList<>();
+    traits1.add("HAIRY");
+    traits1.add("TALL");
+    individual1.setId(1);
+    individual1.setAge(25);
+    individual1.setHumanoid(false);
+    individual1.setPlanet("Kashyyk");
+    individual1.setTraits(traits1);
+
+    System.out.println("\nIndividual nr. " + individual1.getId() + " :");
+    System.out.println("\nId: "+individual1.getId());
+    System.out.println("\nAge: "+individual1.getAge());
+    System.out.println("\nHumanoid: "+individual1.isHumanoid());
+    System.out.println("\nPlanet: "+individual1.getPlanet());
+    System.out.println("\nTraits: "+individual1.getTraits());
+
+  }
 }
 
-record Universe(
+/*
+ record Universe(
     String name,
     List<JsonNode> individuals
 ) { }
+ */
