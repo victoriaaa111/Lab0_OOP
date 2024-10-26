f45c1293c86477d7025e4af02f579b7d1494f2f9
victoriaaa111 <victoria.mutruc@gmail.com>
2024-10-27 00:55:01 +0300

    Finish task 1. Create display class, 3 display objects and compare them using methods from display class

---
+++ src/main/java/Lab1/Task1/Display.java
@@ -0,0 +1,71 @@
+package Lab1.Task1;
+
+public class Display {
+    private int width;
+    private int height;
+    private float ppi;
+    private String model;
+
+   public Display(int width, int height, float ppi, String model) {
+       this.width = width;
+       this.height = height;
+       this.ppi = ppi;
+       this.model = model;
+   }
+
+    public void compareSize(Display m){
+        int thisArea = this.width*this.height;
+        int mArea = m.width*m.height;
+        if(thisArea < mArea){
+            System.out.println("    The size of the " + m.model + " is bigger than the size of the " + this.model + " display");
+        }else if(thisArea > mArea){
+            System.out.println("    The size of the " + this.model + " is bigger than the size of the " + m.model + " display");
+        }else{
+            System.out.println("    The size of the " + this.model + " and "+ m.model + " displays is equal");
+        }
+    }
+    void compareSharpness(Display m){
+        if(this.ppi < m.ppi){
+            System.out.println("    The size of the " + m.ppi + " is bigger than the size of the " + this.ppi + " display");
+        }else if(this.ppi > m.ppi){
+            System.out.println("    The size of the " + this.ppi + " is bigger than the size of the " + m.ppi + " display");
+        }else{
+            System.out.println("    The size of the " + this.ppi + " and "+ m.ppi + " displays is equal");
+        }
+    }
+    void compareWithMonitor(Display m){
+        System.out.println("\n By size:\n");
+        compareSize(m);
+        System.out.println("\n By sharpness:\n");
+        compareSharpness(m);
+    }
+
+    public int getWidth() {
+       return width;
+    }
+    public void setWidth(int width) {
+       this.width = width;
+    }
+
+    public int getHeight() {
+       return height;
+    }
+    public void setHeight(int height) {
+       this.height = height;
+    }
+
+    public float getPpi() {
+       return ppi;
+    }
+    public void setPpi(float ppi) {
+       this.ppi = ppi;
+    }
+
+    public String getModel() {
+       return model;
+    }
+    public void setModel(String model) {
+       this.model = model;
+    }
+
+}
