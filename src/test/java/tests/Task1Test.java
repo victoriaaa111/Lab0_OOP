fba5a21f571c22259b0f2345fa865db39e9e36f1
victoriaaa111 <victoria.mutruc@gmail.com>
2024-11-26 23:03:46 +0200

    Task 2 done: Add test for counting individuals and cars for dining/refueling

--- src/test/java/tests/Task1Test.java
+++ src/test/java/tests/Task1Test.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-public class QueueTest {
+public class Task1Test {
     @Test
     void testArrayImplementation() {
         ArrayImplementation<Integer> queue = new ArrayImplementation<>();
