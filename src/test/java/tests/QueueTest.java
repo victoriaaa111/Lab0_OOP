8f644cd85d4f86c34ac623f330a24f8e43d21dfc
victoriaaa111 <victoria.mutruc@gmail.com>
2024-11-26 21:00:11 +0200

    Task 1 done: Add tests for all 3 implementations to check queue, dequeue, peek, isEmpty, size methods

---
+++ src/test/java/tests/QueueTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import Lab3.Task1.ArrayImplementation;
+import Lab3.Task1.DoubleStackImplementation;
+import Lab3.Task1.LinkedListImplementation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class QueueTest {
+    @Test
+    void testArrayImplementation() {
+        ArrayImplementation<Integer> queue = new ArrayImplementation<>();
+        Assertions.assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        Assertions.assertEquals(3, queue.size());
+        Assertions.assertEquals(1, queue.peek());
+        queue.dequeue();
+        Assertions.assertEquals(2, queue.size());
+        queue.dequeue();
+        queue.dequeue();
+        Assertions.assertEquals(0, queue.size());
+    }
+
+    @Test
+    void testLinkedListImplementation() {
+        LinkedListImplementation<String> queue = new LinkedListImplementation<>();
+        Assertions.assertTrue(queue.isEmpty());
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+
+        Assertions.assertEquals(3, queue.size());
+        Assertions.assertEquals("a", queue.peek());
+        queue.dequeue();
+        Assertions.assertEquals(2, queue.size());
+        queue.dequeue();
+        queue.dequeue();
+        Assertions.assertEquals(0, queue.size());
+    }
+    @Test
+    void testDoubleStackImplementation() {
+        DoubleStackImplementation<Integer> queue = new DoubleStackImplementation<>();
+        Assertions.assertTrue(queue.isEmpty());
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        Assertions.assertEquals(3, queue.size());
+        queue.dequeue();
+        Assertions.assertEquals(2, queue.peek());
+        Assertions.assertEquals(2, queue.size());
+        queue.dequeue();
+        Assertions.assertEquals(1, queue.size());
+        queue.dequeue();
+        Assertions.assertEquals(0, queue.size());
+    }
+}
